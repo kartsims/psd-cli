f50527b0287df1ebe1377557b2fb7db162da95cd
Simon <kartsims@gmail.com>
2015-11-27 16:19:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# PSD from CLI
+
+Convert and preview Photoshop files using the CLI.
+
+Powered by NodeJS
+
+## Install
+
+```
+npm install -g psd-cli
+```
+
+## Usage
+
+### Single file conversion
+
+```
+psd path/to/my_psd_file.psd
+# Creates path/to/my_psd_file.png
+```
+
+### Multiple file conversion
+
+```
+psd path/to/my_psd_file.psd another/psd_file.psd
+# Creates path/to/my_psd_file.png and another/psd_file.png
+```
+
+### File conversion and opening
+
+```
+psd -o path/to/my_psd_file.psd
+# Creates path/to/my_psd_file.png and opens it in your default image preview software
+```
+
+## Help
+
+Full usage available by typing `psd --help` 
+
+```
+  Usage: psd [options] <file...>
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+    -o, --open     Preview file after conversion
+```
---
+++ index.js
@@ -0,0 +1,70 @@
+var PSD = require('psd');
+var cp = require('child_process');
+var async = require('async');
+var fileType = require('file-type');
+var readChunk = require('read-chunk');
+
+var filesProcessed = [];
+
+// setup Commander program
+require('commander')
+  .version(require('./package.json').version)
+  .arguments('<file...>')
+  .option('-o, --open', 'Preview file after conversion')
+  .action(processFiles)
+  .parse(process.argv);
+
+
+// here lies the PSD magic
+function processFiles(files, env) {
+  async.each(files, function(file, cb) {
+
+    try {
+      var buffer = readChunk.sync(file, 0, 262);
+      var type = fileType(buffer).ext;
+      if (type!='psd') {
+        console.log(file, "is not a PSD file, type detected :", type);
+        return cb();
+      }
+    }
+    catch (e) {
+      console.log(file, "could not be opened with PSD library");
+      return cb();
+    }
+
+    var filePng = file.replace(/\.psd$/, '.png');
+
+    PSD.open(file).then(function (psd) {
+      return psd.image.saveAsPng(filePng);
+    }).then(function (err) {
+      console.log('PNG saved to', filePng);
+      filesProcessed.push(filePng);
+      return cb();
+    });
+  }, processDone);
+}
+
+
+function processDone(err) {
+  if (err) {
+    console.log("Error processing the files", err);
+  }
+  
+  console.log("Files processed successfully", filesProcessed);
+
+  if (program.open) {
+    var commandLine = getCommandLine();
+    console.log('Opening files command :', commandLine);
+    cp.spawn(commandLine, filesProcessed, { detached: true })
+      .unref();
+  }
+}
+
+function getCommandLine() {
+   switch (process.platform) { 
+      case 'darwin' : return 'open';
+      case 'win32' : return 'start';
+      case 'win64' : return 'start';
+      default : return 'xdg-open';
+   }
+}
